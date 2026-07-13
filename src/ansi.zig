e03e87cf0bd6d59ed59443885f91bd0b8e67d493
Christian Helle <christian.helle@outlook.com>
2026-07-14 00:23:32 +0200

    Add green, yellow, and cyan ANSI color codes

--- src/ansi.zig
+++ src/ansi.zig
@@ -1,3 +1,6 @@
 pub const reset = "\x1b[0m";
 pub const dim = "\x1b[2m";
 pub const bright = "\x1b[1;37m";
+pub const green = "\x1b[32m";
+pub const yellow = "\x1b[33m";
+pub const cyan = "\x1b[36m";
