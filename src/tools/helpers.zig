054e11d3ab5851ff048d3c43469266cd31937d8d
Christian Helle <christian.helle@outlook.com>
2026-07-31 10:27:10 +0200

    verify Content-Length header against downloaded file size

--- src/tools/helpers.zig
+++ src/tools/helpers.zig
@@ -124,6 +124,14 @@ pub fn httpDownloadFile(allocator: std.mem.Allocator, io: std.Io, url: []const u
     if (result.status != .ok) {
         return error.HttpNotOk;
     }
+
+    if (result.content_length) |expected| {
+        const stat = try dest_dir.statFile(io, dest_name);
+        if (stat.size != expected) {
+            dest_dir.deleteFile(io, dest_name) catch {};
+            return error.TruncatedDownload;
+        }
+    }
 }
 
 pub fn httpGet(allocator: std.mem.Allocator, io: std.Io, url: []const u8) ![]const u8 {
@@ -227,4 +235,31 @@ test "retryDownload fails immediately on non-transient error" {
     const result = retryDownload(std.testing.allocator, std.testing.io, "http://example.com/test", tmp_dir.dir, "test.zip", random, testDownload);
     try std.testing.expectEqual(@as(usize, 1), test_download_attempts);
     _ = result;
+}
+
+fn validateDownloadSize(io: std.Io, dest_dir: std.Io.Dir, dest_name: []const u8, expected: u64) !void {
+    const stat = try dest_dir.statFile(io, dest_name);
+    if (stat.size != expected) return error.TruncatedDownload;
+}
+
+test "validateDownloadSize accepts matching size" {
+    var tmp_dir = std.testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
+    var file = try tmp_dir.dir.createFile(std.testing.io, "test.bin", .{});
+    defer file.close(std.testing.io);
+    var buf: [8]u8 = .{1} ** 8;
+    try file.writeStreamingAll(std.testing.io, &buf);
+    file.close(std.testing.io);
+    try validateDownloadSize(std.testing.io, tmp_dir.dir, "test.bin", 8);
+}
+
+test "validateDownloadSize rejects mismatched size" {
+    var tmp_dir = std.testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
+    var file = try tmp_dir.dir.createFile(std.testing.io, "test.bin", .{});
+    defer file.close(std.testing.io);
+    var buf: [4]u8 = .{1} ** 4;
+    try file.writeStreamingAll(std.testing.io, &buf);
+    file.close(std.testing.io);
+    try std.testing.expectError(error.TruncatedDownload, validateDownloadSize(std.testing.io, tmp_dir.dir, "test.bin", 8));
 }
