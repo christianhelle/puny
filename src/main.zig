31aeefaa25ab0845c4e0829839b806a1bdf21441
Christian Helle <christian.helle@outlook.com>
2026-08-04 00:26:07 +0200

    add DebugLog printBody that writes formatted JSON bodies

--- src/main.zig
+++ src/main.zig
@@ -60,6 +60,7 @@ pub fn main(init: std.process.Init) !void {
         break :blk DebugLog{
             .file = file,
             .writer = &debug_file_writer.interface,
+            .allocator = arena,
         };
     } else null;
     defer if (debug_log) |*log| {
