d602db431af96bfc8cf7d5c19d6e73cda7503f41
Christian Helle <christian.helle@outlook.com>
2026-08-15 16:46:20 +0200

    remove private helper re-exports from sessions facade

--- src/sessions/sessions.zig
+++ src/sessions/sessions.zig
@@ -1,14 +1,10 @@
 const types = @import("types.zig");
-const validate = @import("validate.zig");
 const index = @import("index.zig");
 const query = @import("query.zig");
 
 pub const SessionInfo = types.SessionInfo;
 pub const dupeSessionInfo = types.dupeSessionInfo;
 pub const lessThan = types.lessThan;
-
-pub const isValidSessionId = validate.isValidSessionId;
-pub const truncateFirstPrompt = validate.truncateFirstPrompt;
 
 pub const sessionsPath = index.sessionsPath;
 pub const listSessions = index.listSessions;
